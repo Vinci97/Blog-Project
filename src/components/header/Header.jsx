c94c2954f886a92ddf2b7d6d625e8bd0598a4566
Vincenzo Costantino <vincenzo.costantino04@gmail.com>
2024-04-09 00:29:22 +0200

    librerie2.0

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -14,12 +14,12 @@ const Header = ({ isOpen, toggleMenu }) => {
         <button className={styles.close} onClick={toggleMenu}>
         </button>
       </div>
-      <Link href="/" className={styles.argomento} onClick={toggleMenu}>HOME</Link>
-      <Link href="/Criticainternazionale" className={styles.argomento} onClick={toggleMenu}>STUDI INTERNAZIONALI</Link>
-      <Link href="/Politicaesoceta" className={styles.argomento} onClick={toggleMenu}>POLITICA E SOCIETÀ</Link>
-      <Link href="/Controinformazione" className={styles.argomento} onClick={toggleMenu}>CULTURA E TERRITORIO</Link>
-      <Link href="/OpenDiritto" className={styles.argomento} onClick={toggleMenu}>FAKE NEWS</Link>
-      <Link href="/ComeFare" className={styles.argomento} onClick={toggleMenu}>COME FARE...?</Link>
+      <Link href="/" className={styles.argomento}>HOME</Link>
+      <Link href="/Criticainternazionale" className={styles.argomento}>CRITICA INTERNAZIONALE</Link>
+      <Link href="/Politicaesocieta" className={styles.argomento}>POLITICA, GIOVANI E SOCIETÀ</Link>
+      <Link href="/Controinformazione" className={styles.argomento}>CONTROINFORMAZIONE</Link>
+      <Link href="/OpenDiritto" className={styles.argomento}>OPEN DIRITTO</Link>
+      <Link href="/ComeFare" className={styles.argomento}>COME FARE...?</Link>
     </div>
   );
 };
