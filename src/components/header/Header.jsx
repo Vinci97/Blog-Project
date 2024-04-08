092ee3fb22f44c573a0ef4ae29ba1d6f7f27fe3c
Vincenzo Costantino <vincenzo.costantino04@gmail.com>
2024-04-09 01:15:18 +0200

    debug

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -15,8 +15,8 @@ const Header = ({ isOpen, toggleMenu }) => {
         </button>
       </div>
       <Link href="/" className={styles.argomento}>HOME</Link>
-      <Link href="/Criticainternazionale" className={styles.argomento}>CRITICA INTERNAZIONALE</Link>
-      <Link href="/Politicaesocieta" className={styles.argomento}>POLITICA, GIOVANI E SOCIETÀ</Link>
+      <Link href="/Criticainternazionale" className={styles.argomento}><span className={styles.argSpan}>CRITICA<span>INTERNAZIONALE</span></span></Link>
+      <Link href="/Politicaesocieta" className={styles.argomento}><span className={styles.argSpan}>POLITICA, <span>GIOVANI E SOCIETÀ</span> </span></Link>
       <Link href="/Controinformazione" className={styles.argomento}>CONTROINFORMAZIONE</Link>
       <Link href="/OpenDiritto" className={styles.argomento}>OPEN DIRITTO</Link>
       <Link href="/ComeFare" className={styles.argomento}>COME FARE...?</Link>
