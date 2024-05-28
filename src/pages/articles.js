c5c98c733a3d2d28a14668eea04cc9b6ea06b977
vincenzo <vinccenzo.costantino04@gmail.com>
2024-05-28 19:56:07 +0200

    d

--- src/pages/articles.js
+++
@@ -1,7 +0,0 @@
-import articlesData from '../../../data/articles.json'; // Assicurati di avere questo file con i dati degli articoli
-
-export default function handler(req, res) {
-  const { category } = req.query;
-  const filteredArticles = articlesData.filter(article => article.category === category);
-  res.status(200).json(filteredArticles);
-}
