cf549f551e375ac42a70932240a2dce96a05db17
vincenzo <vinccenzo.costantino04@gmail.com>
2024-06-02 21:42:38 +0200

    a

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  siteUrl: 'https://www.tuosito.com', // Sostituisci con il tuo dominio
+  siteUrl: 'https://www.il-blog-di-michele-vacca.it/', // Sostituisci con il tuo dominio
   generateRobotsTxt: true, // Genera anche il file robots.txt
 }
