670270a78462862d91b6a8c1bf79939963e2e8b3
vincenzo <vinccenzo.costantino04@gmail.com>
2024-06-02 12:41:20 +0200

    a

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -12,8 +12,8 @@ const Footer = ()=> {
                 <div className={styles.footerIconeSocial}>
                     <SocialIcon className={styles.icon} network="tiktok" bgColor="rgba(0, 0, 0, 0)"style={{ height: 34, width: 34 }} url="https://www.tiktok.com/@michele.vacca.blog?_t=8mrJ15DgGkS&_r=1"target="_blank"rel="noopener noreferrer"/>
                     <SocialIcon className={styles.icon} network="youtube" bgColor="rgba(0, 0, 0, 0)"style={{ height: 34, width: 34}} url="https://www.youtube.com/channel/UC_a5LsGJWmwSCDoV0SbD9AQ"target="_blank"rel="noopener noreferrer"/>
-                    <SocialIcon className={styles.icon} network="instagram" bgColor="rgba(0, 0, 0, 0)"style={{ height: 34, width: 34 }}url="https://www.instagram.com/vacca_michele/"target="_blank"rel="noopener noreferrer"/>
-                    <SocialIcon className={styles.icon} network="facebook" bgColor="rgba(0, 0, 0, 0)"style={{ height: 34, width: 34}} url="https://www.facebook.com/michele.vacca.99"target="_blank"rel="noopener noreferrer"/>
+                    <SocialIcon className={styles.icon} network="instagram" bgColor="rgba(0, 0, 0, 0)"style={{ height: 34, width: 34 }}url="https://www.instagram.com/blogdimichelevacca/?igsh=MXBiMHh4NnB2NGtubQ%3D%3D"target="_blank"rel="noopener noreferrer"/>
+                    <SocialIcon className={styles.icon} network="facebook" bgColor="rgba(0, 0, 0, 0)"style={{ height: 34, width: 34}} url="https://www.facebook.com/profile.php?id=100063728582740"target="_blank"rel="noopener noreferrer"/>
                 </div>
             </div>
         </div>
--- src/pages/article/[id].jsx
+++ src/pages/article/[id].jsx
@@ -66,8 +66,8 @@ const ArticlePage = () => {
         <div className={styles.social}>
           <SocialIcon className={styles.icon} network="tiktok" bgColor="rgba(9, 9, 9, 9)"style={{ height: 34, width: 34 }} url="https://www.tiktok.com/@michele.vacca.blog?_t=8mrJ15DgGkS&_r=1"target="_blank"rel="noopener noreferrer"/>
           <SocialIcon className={styles.icon} network="youtube" bgColor="rgba(9, 9, 9, 9)"style={{ height: 34, width: 34}} url="https://www.youtube.com/channel/UC_a5LsGJWmwSCDoV0SbD9AQ"target="_blank"rel="noopener noreferrer"/>
-          <SocialIcon className={styles.icon} network="instagram" bgColor="rgba(9, 9, 9, 9)"style={{ height: 34, width: 34 }}url="https://www.instagram.com/vacca_michele/"target="_blank"rel="noopener noreferrer"/>
-          <SocialIcon className={styles.icon} network="facebook" bgColor="rgba(9, 9, 9, 9)"style={{ height: 34, width: 34}} url="https://www.facebook.com/michele.vacca.99"target="_blank"rel="noopener noreferrer"/>
+          <SocialIcon className={styles.icon} network="instagram" bgColor="rgba(9, 9, 9, 9)"style={{ height: 34, width: 34 }}url="https://www.instagram.com/blogdimichelevacca/?igsh=MXBiMHh4NnB2NGtubQ%3D%3D"target="_blank"rel="noopener noreferrer"/>
+          <SocialIcon className={styles.icon} network="facebook" bgColor="rgba(9, 9, 9, 9)"style={{ height: 34, width: 34}} url="https://www.facebook.com/profile.php?id=100063728582740"target="_blank"rel="noopener noreferrer"/>
         </div>
         <br/>
         <div>{contenutoParagrafi}</div>
