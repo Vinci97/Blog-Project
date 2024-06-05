a1db7f0445854390046f827da6383d1516967b7d
vincenzo <vinccenzo.costantino04@gmail.com>
2024-06-05 02:11:06 +0200

    a

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,22 +1,47 @@
-import { SocialIcon } from "react-social-icons"
-import styles from "./footer.module.scss"
-const Footer = ()=> {
-    return(
+import { SocialIcon } from "react-social-icons";
+import styles from "./footer.module.scss";
+import { useState, useEffect } from "react";
+
+const Footer = () => {
+    const [iconSize, setIconSize] = useState(34);
+
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth >= 768) {
+                setIconSize(50);
+            } else {
+                setIconSize(34);
+            }
+        };
+
+        window.addEventListener('resize', handleResize);
+        handleResize();
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    return (
         <div className={styles.Footer}>
-            <div className={styles.footerContatti}>
-                <h3>CONTATTI</h3>
-                <p className={styles.footerTxtContatti}><span>Michele Vacca:</span> web@MicheleVacca.it</p>
+            <div className={styles.container1}>
+                <div className={styles.titoli}>
+                    <h3>CONTATTI</h3>
+                </div>
+                <div className={styles.footerContatti}>
+                    <a href="mailto:postmaster@il-blog-di-michele-vacca.it">@il-blog-di-michele-vacca.it</a>
+                </div>
             </div>
-            <div className={styles.footerSocial}>
-                <h3>SEGUIMI</h3>
-                <div className={styles.footerIconeSocial}>
-                    <SocialIcon className={styles.icon} network="tiktok" bgColor="rgba(0, 0, 0, 0)"style={{ height: 34, width: 34 }} url="https://www.tiktok.com/@michele.vacca.blog?_t=8mrJ15DgGkS&_r=1"target="_blank"rel="noopener noreferrer"/>
-                    <SocialIcon className={styles.icon} network="youtube" bgColor="rgba(0, 0, 0, 0)"style={{ height: 34, width: 34}} url="https://www.youtube.com/channel/UC_a5LsGJWmwSCDoV0SbD9AQ"target="_blank"rel="noopener noreferrer"/>
-                    <SocialIcon className={styles.icon} network="instagram" bgColor="rgba(0, 0, 0, 0)"style={{ height: 34, width: 34 }}url="https://www.instagram.com/blogdimichelevacca/?igsh=MXBiMHh4NnB2NGtubQ%3D%3D"target="_blank"rel="noopener noreferrer"/>
-                    <SocialIcon className={styles.icon} network="facebook" bgColor="rgba(0, 0, 0, 0)"style={{ height: 34, width: 34}} url="https://www.facebook.com/profile.php?id=100063728582740"target="_blank"rel="noopener noreferrer"/>
+            <div className={styles.container2}>
+                <div className={styles.titoli}>
+                    <h3>SEGUIMI</h3>
+                </div>
+                <div className={styles.footerSocial}>
+                    <SocialIcon className={styles.icon} network="tiktok" bgColor="rgba(0, 0, 0, 0)" style={{ height: iconSize, width: iconSize }} url="https://www.tiktok.com/@michele.vacca.blog?_t=8mrJ15DgGkS&_r=1" target="_blank" rel="noopener noreferrer" />
+                    <SocialIcon className={styles.icon} network="youtube" bgColor="rgba(0, 0, 0, 0)" style={{ height: iconSize, width: iconSize }} url="https://www.youtube.com/channel/UC_a5LsGJWmwSCDoV0SbD9AQ" target="_blank" rel="noopener noreferrer" />
+                    <SocialIcon className={styles.icon} network="instagram" bgColor="rgba(0, 0, 0, 0)" style={{ height: iconSize, width: iconSize }} url="https://www.instagram.com/blogdimichelevacca/?igsh=MXBiMHh4NnB2NGtubQ%3D%3D" target="_blank" rel="noopener noreferrer" />
+                    <SocialIcon className={styles.icon} network="facebook" bgColor="rgba(0, 0, 0, 0)" style={{ height: iconSize, width: iconSize }} url="https://www.facebook.com/profile.php?id=100063728582740" target="_blank" rel="noopener noreferrer" />
                 </div>
             </div>
         </div>
-    )
-}
-export default Footer
+    );
+};
+
+export default Footer;
