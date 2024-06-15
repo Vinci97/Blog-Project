5977bf16589a17ab32887f9561dc1195bf64d49f
vincenzo <vinccenzo.costantino04@gmail.com>
2024-06-15 17:25:28 +0200

    a

--- src/pages/ComeFare.jsx
+++ src/pages/ComeFare.jsx
@@ -2,9 +2,10 @@ import { useState } from "react"
 import styles from "../styles/ComeFare.module.scss"
 import ArticlesByCategory from "@/components/ArticlesByCategory";
 import Head from "next/head"
-import Header from "@/components/Header";
-import Hamburger from "@/components/Hamburger";
-import Navbar from "@/components/Navbar";
+import Header from "@/components/header";
+import Hamburger from "@/components/hamburger";
+import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
 
 const ComeFare = () => {
     const [menuAperto, setMenuAperto] = useState(false);
