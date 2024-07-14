20db6f1b368495cf572640b55ab7943b6fa5085d
vincenzo <vinccenzo.costantino04@gmail.com>
2024-07-14 12:33:54 +0200

    a

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  siteUrl: 'https://www.il-blog-di-michele-vacca.it/', // Sostituisci con il tuo dominio
-  generateRobotsTxt: true, // Genera anche il file robots.txt
-  exclude: ['/api/*'], // Escludi le rotte API dalla sitemap
+  siteUrl: 'https://www.il-blog-di-michele-vacca.it/', 
+  generateRobotsTxt: true, 
+  exclude: ['/api/*'], 
   changefreq: 'daily',
   priority: 0.7,
   sitemapSize: 5000,
-  outDir: 'public', // Directory di output per sitemap e robots.txt
+  outDir: 'public',
 }
